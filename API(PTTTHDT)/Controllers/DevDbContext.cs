1a8f6685a4ec26f483b6ec6797b24bf36111b487
trungnguyendev <trungvjppro23@gmail.com>
2023-10-22 14:48:31 +0700

    5

---
+++ API(PTTTHDT)/Controllers/DevDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_PTTTHDT_.Controllers
+{
+    public class DevDbContext : DbContext
+    {
+        public DevDbContext(DbContextOptions<DevDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
